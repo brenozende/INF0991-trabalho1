00b5d5ec54e13ae30966dacb084f531b8464be50
Breno Rezende Pinto <71926860+brenozende@users.noreply.github.com>
2022-08-31 17:03:18 -0300

    Código fonte adicionado

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+// Seção 4.1:
+// Exercício 1
+
+// string valor = null;
+// valor = valor?.Trim().ToUpper();
+// Console.WriteLine(valor);
+
+// int tamanho = (int?)pessoa?.Length;
+
+// Seção 4.2
+// Exercício 1
+
+// int i;
+// Console.WriteLine(i); // Dispara erro de variável local não atribuída.
+
+// Exercício 2
+
+// const double c = 2.99792458E08;
+// c = c + 2;  // Erro de build dizendo que o lado esquerdo deve conter uma variavel, propriedade ou indexador. Logo, constante não funciona.
+// Console.WriteLine(c);
+
+// Exercício 3
+
+// char letra = 'z';
+// letra++;
+// Console.WriteLine(letra); // O programa passa para o próximo caractere na tabela ascii
